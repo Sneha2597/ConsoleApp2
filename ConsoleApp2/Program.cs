92d757c4fe00a2e075113e45cb8d8ea332ee35f7
Sneha Aandal Mutha <srisneha1230@gmail.com>
2020-07-28 16:36:23 +0530

    Testing the merge

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Gitflow feature");
+            Console.WriteLine("Feature-1 again");
+            Console.WriteLine("Merging the feature with develop");
         }
     }
 }
